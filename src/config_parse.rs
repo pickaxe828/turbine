a8d886d4dd4146823896daac8d0aa3152a91d926
Pickaxe828 <joshuajcn828@gmail.com>
2022-12-15 23:45:07 +0800

    ✨ Fix

--- src/config_parse.rs
+++ src/config_parse.rs
@@ -1,9 +1,10 @@
 use std::collections::HashMap;
 use serde_derive::{Deserialize};
+use toml::Value;
 
 #[derive(Deserialize)]
 pub struct Config {
     pub input: HashMap<String, String>,
-    pub behaviour: HashMap<String, String>,
+    pub behaviour: HashMap<String, Value>,
     pub rules: HashMap<String, String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use std::{fs};
-use toml;
+use std::{fs, collections::HashMap};
+use toml::{self, Value};
+use std::{thread, time::Duration};
+
 mod config_parse;
+
+fn text_to_squares(input: &String) -> String {
+    input.replace("0", "  ").replace("1", "██")
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect::<Vec<_>>();
@@ -11,15 +17,32 @@ fn main() {
 
     // Read config file
     let config_raw = fs::read_to_string("./config.turbine.toml").unwrap();
-    let config: config_parse::Config = toml::from_str(&config_raw).unwrap();
+    let config = &config_raw.parse::<Value>().unwrap();
 
-    let rules = config.rules;
-    let input = config.input;
+    let mut input = String::from(config["input"]["tape"].as_str().unwrap());
+    let behaviour = config["behaviour"].as_table().unwrap();
+    let rules = config["rules"].as_table().unwrap();
+
+    let reader_width = behaviour.get("reader_width").unwrap().as_integer().unwrap() as usize;
     
-    let mut output: Vec<String> = Vec::new();
-    for i in 0..input.len()-2 {
-        let instruction = &input[&(i..i+3)];
-        let out = rules[instruction].to_string();
-        output.push(out);
+    // check if all the tape, rules and the reader is valid
+    if reader_width > input.chars().count() {
+        panic!("Reader width is larger than the tape length.");
+    }
+
+    // how stuff begins
+    // **Mechanical Squeaking noise**
+    println!("{}", text_to_squares(&input));
+    loop {
+        let mut result: Vec<String> = Vec::new();
+        input.chars().into_iter().enumerate().for_each(|(ind, val)| {
+            if ind < (input.chars().count() - reader_width + 1) {
+                result.push(rules.get(&input.get(ind..ind + reader_width).unwrap().to_string()).unwrap().to_string());
+            }
+        });
+        let output = format!("0{}0", result.concat());
+        println!("{}", text_to_squares(&output));
+        input = output;
+        thread::sleep(Duration::from_millis(100));
     }
 }
